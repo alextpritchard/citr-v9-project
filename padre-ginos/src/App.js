05b2fc6147fce5ac78c08fca358ee4faba731b6e
Alex <4258252+alextpritchard@users.noreply.github.com>
2025-03-29 21:32:34 +0000

    init project

---
+++ padre-ginos/src/App.js
@@ -0,0 +1,37 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div",
+
+    {},
+    [
+      React.createElement("h1", {}, "Padre Gino's"),
+      React.createElement(Pizza, {
+        name: "Margherita",
+        description: "Tomato, mozzarella, and basil",
+      }),
+      React.createElement(Pizza, {
+        name: "Pepperoni",
+        description: "Tomato, mozzarella, and pepperoni",
+      }),
+      React.createElement(Pizza, {
+        name: "Hawaiian",
+        description: "Tomato, mozzarella, ham, and pineapple",
+      }),
+      React.createElement(Pizza, {
+        name: "Vegetarian",
+        description: "Tomato, mozzarella, and assorted vegetables",
+      }),
+    ],
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
