858330273da0902daafa164d79f8adc20f836c78
Alex <4258252+alextpritchard@users.noreply.github.com>
2025-03-29 22:37:55 +0000

    Add images

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -6,15 +6,25 @@ const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order now</h1>
-      <Pizza name="Margherita" description="Tomato, mozzarella, and basil" />
-      <Pizza name="Pepperoni" description="Tomato, mozzarella, and pepperoni" />
+      <Pizza
+        name="Margherita"
+        description="BBQ Chicken"
+        image={"/public/pizzas/bbq_ckn.webp"}
+      />
+      <Pizza
+        name="Pepperoni"
+        description="Tomato, mozzarella, and pepperoni"
+        image={"/public/pizzas/pepperoni.webp"}
+      />
       <Pizza
         name="Hawaiian"
         description="Tomato, mozzarella, ham, and pineapple"
+        image={"/public/pizzas/hawaiian.webp"}
       />
       <Pizza
         name="Vegetarian"
         description="Tomato, mozzarella, and assorted vegetables"
+        image={"/public/pizzas/mediterraneo.webp"}
       />
     </div>
   );
