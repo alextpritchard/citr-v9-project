1d3ea4382b82397238db17cf4ee615939a02251b
Alex <4258252+alextpritchard@users.noreply.github.com>
2025-03-31 23:43:53 +0100

    Add custom hook

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -2,6 +2,7 @@ import { StrictMode, strictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Order from "./Order";
+import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div>
         <h1>Padre Gino's - Order now</h1>
         <Order />
+        <PizzaOfTheDay />
       </div>
     </StrictMode>
   );
---
+++ padre-ginos/src/usePizzaOfTheDay.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+export const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const response = await fetch("/api/pizza-of-the-day");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
