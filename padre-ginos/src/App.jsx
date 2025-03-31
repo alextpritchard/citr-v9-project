eefded3b9db43381b9c7786378db547cb2590de9
Alex <4258252+alextpritchard@users.noreply.github.com>
2025-03-31 22:26:52 +0100

    Add Order component

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,31 +1,12 @@
 import { createRoot } from "react-dom/client";
 
-import Pizza from "./Pizza.jsx";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order now</h1>
-      <Pizza
-        name="Margherita"
-        description="BBQ Chicken"
-        image={"/public/pizzas/bbq_ckn.webp"}
-      />
-      <Pizza
-        name="Pepperoni"
-        description="Tomato, mozzarella, and pepperoni"
-        image={"/public/pizzas/pepperoni.webp"}
-      />
-      <Pizza
-        name="Hawaiian"
-        description="Tomato, mozzarella, ham, and pineapple"
-        image={"/public/pizzas/hawaiian.webp"}
-      />
-      <Pizza
-        name="Vegetarian"
-        description="Tomato, mozzarella, and assorted vegetables"
-        image={"/public/pizzas/mediterraneo.webp"}
-      />
+      <Order />
     </div>
   );
 };
