64d13c635f25a0dbb633b026b87b1860f05d2020
Alex <4258252+alextpritchard@users.noreply.github.com>
2025-04-04 21:13:43 +0100

    update ref

--- padre-ginos/src/Modal.jsx
+++ padre-ginos/src/Modal.jsx
@@ -8,7 +8,7 @@ const Modal = ({ children }) => {
   }
 
   useEffect(() => {
-    const modalRoot = document.getElementById("modal-root");
+    const modalRoot = document.getElementById("modal");
     modalRoot.appendChild(elRef.current);
 
     return () => {
